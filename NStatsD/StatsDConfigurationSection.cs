90d50d1feef6b57f5ed803a8202d9db937478dee
Rob Bihun <rob@robbihun.com>
2012-06-01 18:21:34 -0500

    attempting to get the nupackage to do config transforms

---
+++ NStatsD/StatsDConfigurationSection.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+
+namespace NStatsD
+{
+    public class StatsDConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationProperty("server")]
+        public ServerElement Server
+        {
+            get { return (ServerElement) this["server"]; }
+            set { this["server"] = value; }
+        }
+    }
+
+    
+    public class ServerElement : ConfigurationElement
+    {
+        [ConfigurationProperty("host", DefaultValue = "localhost", IsRequired = true)]
+        public string Host
+        {
+            get { return (string) this["host"]; }
+            set { this["host"] = value; }
+        }
+
+        [ConfigurationProperty("port", DefaultValue = "8125", IsRequired = false)]
+        public int Port
+        {
+            get { return (int) this["port"]; }
+            set { this["port"] = value; }
+        }
+    }
+}
