26d35eaa44ead123790b8cd42ee3d9bae56509a9
Rob Bihun <rob@robbihun.com>
2013-11-14 10:13:01 -0600

    Update AssemblyInfo.cs
    Updated version & copyright.

--- NStatsD/Properties/AssemblyInfo.cs
+++ NStatsD/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rob Bihun")]
 [assembly: AssemblyProduct("NStatsD.Client")]
-[assembly: AssemblyCopyright("Copyright © Rob Bihun 2012")]
+[assembly: AssemblyCopyright("Copyright © Rob Bihun 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
